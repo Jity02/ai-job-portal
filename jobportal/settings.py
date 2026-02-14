a693e40e68c825c943235a1e206ec79eaf9419f6
Chethan-GT <chethangt04@gmail.com>
2026-02-14 13:58:04 +0530

    Switched to SQLite for Render

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -76,15 +76,8 @@ WSGI_APPLICATION = 'jobportal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'jobportal_db',
-        'USER': 'jobuser',
-        'PASSWORD': 'Job@123',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
