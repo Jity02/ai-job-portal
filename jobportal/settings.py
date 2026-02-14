80e3d759533d518d5d89c156fa1b3d569558eb9f
Chethan-GT <chethangt04@gmail.com>
2026-02-14 15:00:13 +0530

    Added PostgreSQL support

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -79,13 +79,11 @@ TEMPLATES = [
 # DATABASE (SQLite Only)
 # ===============================
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+}
 
 # ===============================
 # PASSWORD VALIDATION
