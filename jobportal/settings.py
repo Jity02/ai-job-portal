f5ff05dbc714ed2e719c2fc7d0f1069a5d924327
Chethan-GT <chethangt04@gmail.com>
2026-02-14 15:58:55 +0530

    Adjusted DB connection for free Render

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -93,13 +93,13 @@ if os.environ.get("DATABASE_URL"):
     }
 else:
     # Local development (SQLite)
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-
+   DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=0,  # IMPORTANT
+        ssl_require=True
+    )
+}
 
 # ===============================
 # PASSWORD VALIDATION
